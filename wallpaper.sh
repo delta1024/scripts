48d5842adee12eb909d1b883dd103d3e3904bb9c
Jacob Stannix <jakestannix at gmail dot com>
2021-05-19 20:00:53 -0600

    set for single use wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,27 +1,13 @@
 #!/bin/env bash
 if [[ $1 == "draw" ]]; then
-cd /home/jake/Pictures/Wallpapers
-leftDisplay=$(xrandr | grep " connected" | cut -d" " -f1 | sed -n '1p')
-rightDisplay=$(xrandr | grep " connected" | cut -d" " -f1 | sed -n '2p')
-leftImage=$(cat .background | sed -n '1p')
-rightImage=$(cat .background | sed -n '2p')
-xwallpaper --output ${leftDisplay} --stretch ${leftImage} --output ${rightDisplay} --stretch ${rightImage}
+    cd /home/jake/Pictures/Wallpapers
+    leftDisplay=$(xrandr | grep " connected" | cut -d" " -f1 | sed -n '1p')
+    leftImage=$(< .background)
+    xwallpaper --output ${leftDisplay} --stretch ${leftImage} 
 fi
 
 if [[ $1 == "set" ]]; then
-cd $HOME/Pictures/Wallpapers
-sxiv -rqot * > TEMP
-   if [ "$(wc -l < TEMP)" == 0 ]; then
-   rm $HOME/Pictures/Wallpapers/TEMP
-   fi
-   if [ "$(wc -l < TEMP)" == 1 ]; then
-    sxiv -rqot * >> TEMP
-    mv TEMP .background
+    cd $HOME/Pictures/Wallpapers
+    sxiv -rqot * > .background
     $HOME/.scripts/wallpaper.sh draw
-   fi
-   if [ "$(wc -l < TEMP)" == 2 ]; then
-      mv TEMP .background
-      $HOME/.scripts/wallpaper.sh draw
-   fi
-
 fi
